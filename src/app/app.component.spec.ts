46d3bbafdf223a97b4b6fc1bfd4c7a8789cad208
Marcos Lucas <marcosvaldeni@gmail.com>
2023-05-11 20:09:52 +0100

    Angular 14

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'practice'`, () => {
+  it(`should have as title 'tea-party-proj'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practice');
+    expect(app.title).toEqual('tea-party-proj');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('practice app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tea-party-proj app is running!');
   });
 });
